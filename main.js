6bbf87c644215f6cbba99609f5b2ba66f2f95392
dream <1281725094@qq.com>
2019-10-23 19:59:59 +0800

    AJAX--Promise

--- main.js
+++ main.js
@@ -48,6 +48,7 @@ myButton.addEventListener('click', (e) => {
     )
 })
 
+
 /*
 let request = new XMLHttpRequest() //声明一个 XMLHttpRequest 对象
 request.open('POST', '/xxx')  //配置 request  参数（method、URL），（设置第一部分）
